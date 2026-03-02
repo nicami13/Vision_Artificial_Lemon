270e69293a92c4d5657b90c48fcc12be987eb0a4
undefain <137202367+nicami13@users.noreply.github.com>
2026-03-02 03:26:24 -0500

    Update Medicion.py

--- Medicion.py
+++ Medicion.py
@@ -34,9 +34,9 @@ def detectar_tamano(image_bytes):
     area = cv2.contourArea(largest)
 
     # 🔴 CLASIFICACIÓN REALISTA PARA 18cm DE DISTANCIA
-    if area < 5000:
+    if area < 100:
         size = "PEQUEÑO"
-    elif area < 15000:
+    elif area < 300:
         size = "MEDIANO"
     else:
         size = "GRANDE"
