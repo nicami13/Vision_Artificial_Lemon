d8cef82af9d317bc9a3c48f53f15a0873daede6e
undefain <137202367+nicami13@users.noreply.github.com>
2026-03-02 02:18:38 -0500

    ooo
    ooo

--- WebApi_Lemon.py
+++ WebApi_Lemon.py
@@ -1,36 +1,26 @@
-from fastapi import FastAPI, WebSocket
-import Medicion
+from fastapi import FastAPI
+from pydantic import BaseModel
 from datetime import datetime
 import base64
-from pydantic import BaseModel
+import Medicion
 
 app = FastAPI()
 
 
+# Modelo para recibir la imagen en Base64
 class ImageRequest(BaseModel):
     image: str
 
 
 @app.post("/clasificar")
 async def clasificar(data: ImageRequest):
+    # Decodificar imagen Base64 a bytes
     image_bytes = base64.b64decode(data.image)
 
+    # Procesar imagen
     size, area = Medicion.detectar_tamano(image_bytes)
 
-    return {
-        "id": f"LIM-{int(datetime.utcnow().timestamp())}",
-        "tamano": size,
-        "area": area,
-        "fecha": datetime.utcnow().isoformat()
-    }
-
-
-@app.post("/clasificar")
-async def clasificar(data: ImageRequest):
-    image_bytes = base64.b64decode(data.image)
-
-    size, area = Medicion.detectar_tamano(image_bytes)
-
+    # Respuesta JSON
     return {
         "id": f"LIM-{int(datetime.utcnow().timestamp())}",
         "tamano": size,
