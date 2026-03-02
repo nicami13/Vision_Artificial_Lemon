60f23c6d4a2dd1d4af5a5674c7c20561a1c6d9d3
undefain <137202367+nicami13@users.noreply.github.com>
2026-03-02 02:04:33 -0500

    Probe
    Probe

--- WebApi_Lemon.py
+++ WebApi_Lemon.py
@@ -2,33 +2,24 @@ from fastapi import FastAPI, WebSocket
 import Medicion
 from datetime import datetime
 import base64
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.websocket("/ws/clasificar")
-async def websocket_clasificar(websocket: WebSocket):
-    await websocket.accept()
 
-    while True:
-        # Recibir JSON desde cliente
-        data = await websocket.receive_json()
+class ImageRequest(BaseModel):
+    image: str
 
-        # Obtener Base64
-        base64_img = data["image"]
 
-        # Decodificar a bytes
-        image_bytes = base64.b64decode(base64_img)
+@app.post("/clasificar")
+async def clasificar(data: ImageRequest):
+    image_bytes = base64.b64decode(data.image)
 
-        # Procesar con OpenCV
-        size, area = Medicion.detectar_tamano(image_bytes)
+    size, area = Medicion.detectar_tamano(image_bytes)
 
-        # Crear respuesta
-        response = {
-            "id": f"LIM-{int(datetime.utcnow().timestamp())}",
-            "tamano": size,
-            "area": area,
-            "fecha": datetime.utcnow().isoformat()
-        }
-
-        # Enviar resultado al cliente
-        await websocket.send_json(response)
+    return {
+        "id": f"LIM-{int(datetime.utcnow().timestamp())}",
+        "tamano": size,
+        "area": area,
+        "fecha": datetime.utcnow().isoformat()
+    }
