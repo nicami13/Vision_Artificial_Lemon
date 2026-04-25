041fc68ca283aa39a0b3c2c18756019d4e968633
undefain <137202367+nicami13@users.noreply.github.com>
2026-04-25 17:09:58 -0500

    otherrr

--- Medicion.py
+++ Medicion.py
@@ -43,7 +43,7 @@ def detectar_tamano(image_bytes):
     
     if area < 400:
         size = "PEQUEÑO"
-    elif area < 2100:
+    elif area < 4000:
         size = "MEDIANO"
     else:
         size = "GRANDE"
